05ce517f9a19d745cbaa9fc8c871788b001ffe6c
ahmadzughaer <ahmadzughaer@gmail.com>
2021-11-11 12:02:01 +0200

    ex 1.1

---
+++ Ex1.1-Yes-or-No.js
@@ -0,0 +1,12 @@
+const yesORno = (boolean) => {
+    if (boolean == true) {
+        return 'Yes';
+    }
+
+    else if (boolean == false) {
+        return 'No';
+    }
+    return 'please enter true or false just'
+}
+
+console.log(yesORno('dsff'));
